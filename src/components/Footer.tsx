99bb713a682d119b5e088df37338d3fc699915eb
Ali Askari <syedaliaskarizaidi1@gmail.com>
2024-10-27 14:48:56 -0700

    Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,113 +1,55 @@
 import Link from "next/link";
 import React from "react";
-import { RiLinkedinBoxFill } from "react-icons/ri";
-import { RiGithubFill } from "react-icons/ri";
-import { ImTwitter } from "react-icons/im";
-import { RiFacebookCircleFill } from "react-icons/ri";
+import { FaFacebookF, FaGithub, FaLinkedin, FaTwitter} from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <footer className="bg-black text-white py-4 mt-8">
-      <div className="container mx-auto px-4 md:px-8 lg:px-16 text-center">
-        <section className="mt-8 text-left mb-5">
-          <ol className="list-decimal grid gap-5 list-inside space-y-4 w-[175px] md:w-[223px] md:text-base md:justify-start md:items-center md:grid md:grid-cols-3 md:gap-[430]">
-            <section className="md:w-[180px] w-[150px] block justify-start items-center">
-              <strong className="text-xl">Core Courses</strong>
-              <ul className="list-disc list-inside md:pl-6 flex gap-4 md:text-4xl md:w-[40vh] md:mt-5">
-                <li className="flex justify-start items-center md:mb-2 md:text-sm hover:text-blue-400 mt-5 md:mt-0 text-sm w-fit">
-                  Programming Fundamentals
-                  <Link
-                    href={"https://www.linkedin.com/in/ali-askari-355257308/"}
-                  ></Link>
-                </li>
-              </ul>
-            </section>
-            <div className="md:w-[180px] w-[150px]">
-              <strong className="text-xl">Social Links</strong>
-              <ul className="list-disc list-inside md:pl-6 flex gap-4 md:text-4xl md:w-[40vh] md:mt-5">
-                <li className="flex justify-start items-center md:mb-2 md:text-[40px] text-2xl">
-                  <Link
-                    href={"https://www.linkedin.com/in/ali-askari-355257308/"}
-                  >
-                    <RiLinkedinBoxFill className="hover:bg-blue-700 mt-5 md:mt-0" />
-                  </Link>
-                </li>
-                <li className="flex justify-start items-center md:mb-2 md:text-4xl text-2xl">
-                  <Link
-                    href={"https://github.com/AliAskariGithub"}
-                    className="hover:bg-gray-600 p-1 rounded-full"
-                  >
-                    <RiGithubFill className="md:mt-0 mt-5" />
-                  </Link>
-                </li>
-                <li className="flex justify-start items-center md:mb-2 md:text-4xl text-xl">
-                  <Link
-                    href={
-                      "https://x.com/Syed_Ali_Askari?t=88dxwRm8tvBnkWDEVmZhWg&s=09"
-                    }
-                    className="hover:bg-sky-500 p-2 rounded-full"
-                  >
-                    <ImTwitter className="md:mt-0 mt-5 sm:text-3xl" />
-                  </Link>
-                </li>
-                <li className="flex justify-start items-center md:mb-2 md:text-4xl text-2xl">
-                  <Link
-                    href={
-                      "https://www.facebook.com/profile.php?id=61564881342854"
-                    }
-                    className="hover:bg-blue-600 p-1 rounded-full"
-                  >
-                    <RiFacebookCircleFill className="md:mt-0 mt-5" />
-                  </Link>
-                </li>
-              </ul>
-            </div>
-            <div className="md:w-[180px] w-[150px]">
-              <strong className="text-xl">Social Links</strong>
-              <ul className="list-disc list-inside md:pl-6 flex gap-4 md:text-4xl md:w-[40vh] md:mt-5">
-                <li className="flex justify-start items-center md:mb-2 md:text-[40px] text-2xl">
-                  <Link
-                    href={"https://www.linkedin.com/in/ali-askari-355257308/"}
-                  >
-                    <RiLinkedinBoxFill className="hover:bg-blue-700 mt-5 md:mt-0" />
-                  </Link>
-                </li>
-                <li className="flex justify-start items-center md:mb-2 md:text-4xl text-2xl">
-                  <Link
-                    href={"https://github.com/AliAskariGithub"}
-                    className="hover:bg-gray-600 p-1 rounded-full"
-                  >
-                    <RiGithubFill className="md:mt-0 mt-5" />
-                  </Link>
-                </li>
-                <li className="flex justify-start items-center md:mb-2 md:text-4xl text-xl">
-                  <Link
-                    href={
-                      "https://x.com/Syed_Ali_Askari?t=88dxwRm8tvBnkWDEVmZhWg&s=09"
-                    }
-                    className="hover:bg-sky-500 p-2 rounded-full"
-                  >
-                    <ImTwitter className="md:mt-0 mt-5 sm:text-3xl" />
-                  </Link>
-                </li>
-                <li className="flex justify-start items-center md:mb-2 md:text-4xl text-2xl">
-                  <Link
-                    href={
-                      "https://www.facebook.com/profile.php?id=61564881342854"
-                    }
-                    className="hover:bg-blue-600 p-1 rounded-full"
-                  >
-                    <RiFacebookCircleFill className="md:mt-0 mt-5" />
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </ol>
-        </section>
-        <p className="text-xs md:text-sm md:mt-8">
-          &copy; 2024 E-Learning | All Rights Reserved.
-        </p>
+    <footer className="bg-black text-[rgb(255,255,255,0.7)] py-8">
+      <div className="container mx-auto px-4 md:flex justify-between">
+
+        <div className="mb-6 md:mb-0">
+          <h3 className="font-semibold text-lg text-white">Core Courses</h3>
+          <ul className="mt-2 space-y-2">
+            <Link href={"/ProgrammingFundamentals/01"} className="hover:text-blue-400">Programming Fundamentals</Link><br />
+            <Link href={"/Web20UsingNextJS/02"} className="hover:text-blue-400">Web2 Using Next.js</Link><br />
+            <Link href={"/EarnAsYouLearn/03"} className="hover:text-blue-400">Earn as You Learn</Link><br />
+          </ul>
+        </div>
+
+        <div className="mb-6 md:mb-0">
+          <h3 className="font-semibold text-lg text-white">Advanced Courses</h3>
+          <ul className="mt-2 space-y-2">
+            <Link href={"/Web3AndMetaverse/04"} className="hover:text-blue-400">Web 3 and Metaverse</Link><br />
+            <Link href={"/CloudNativeComputing/04"} className="hover:text-blue-400">Cloud-Native Computing</Link><br />
+            <Link href={"/ArtificialIntelligence/04"} className="hover:text-blue-400">Artificial Intelligence (AI) and Deep Learning</Link><br />
+            <Link href={"/AmbientComputingAndIoT/04"} className="hover:text-blue-400">Ambient Computing and IoT</Link><br />
+            <Link href={"/GenomicsAndBioinformatics/04"} className="hover:text-blue-400">Genomics and Bioinformatics</Link><br />
+            <Link href={"/NetworkProgrammability/04"} className="hover:text-blue-400">Network Programmability and Automation</Link><br />
+          </ul>
+        </div>
+
+        <div>
+          <h3 className="font-semibold text-lg text-white">Social Links</h3>
+          <div className="flex items-center mt-2 space-x-4">
+            <Link href="https://www.facebook.com/profile.php?id=61564881342854">
+              <FaFacebookF className="text-white hover:text-blue-600" size={24} />
+            </Link>
+            <Link href="https://github.com/AliAskariGithub">
+              <FaGithub className="hover:text-gray-400 text-white" size={24} />
+            </Link>
+            <Link href="https://x.com/Syed_Ali_Askari?t=88dxwRm8tvBnkWDEVmZhWg&s=09">
+              <FaTwitter className="text-white hover:text-sky-500" size={24} />
+            </Link>
+            <Link href="edin.com/in/ali-askari-355257308/">
+              <FaLinkedin className="text-white hover:text-blue-700" size={24} />
+            </Link>
+          </div>
+        </div>
       </div>
+      <p className="text-xs md:text-sm text-center mt-5 -mb-5 text-[rgb(255,255,255)]">
+            &copy; 2024 E-Learning | All Rights
+            Reserved.
+          </p>
     </footer>
   );
 };
